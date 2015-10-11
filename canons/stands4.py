2591baa7e6370765504ea2b711089f317aba2aa3
Ryan Baker <ryan.da.baker@gmail.com>
2015-10-11 12:53:44 -0400

    Adds a canonicalizer for STANDS4

---
+++ canons/stands4.py
@@ -0,0 +1,28 @@
+"""
+    Authors:
+    Ryan Baker
+    Matt Beaulieu
+    Anthony Romeo
+"""
+from requests import get
+from bs4 import BeautifulSoup
+
+URI = 'http://www.stands4.com/services/v2/syno.php?uid=4412&tokenid=92XGfHe4TOMhvfEv&word={word}'
+
+
+def canon(word):
+    """Given a word, returns a list of synonyms in order of relevance"""
+    soup = BeautifulSoup(get(URI.format(word=word)).content, 'xml')
+    syns = []
+    syn_lists = [child.text.split(', ') for child in soup.results('synonyms')]
+    while any(syn_lists):
+        for lst in syn_lists:
+            if lst:
+                syns.append(lst[0])
+                lst.remove(lst[0])
+    return syns
+
+
+if __name__ == "__main__":
+    word = raw_input("What word are you looking up? ")
+    canon(str(word))
--- canons/thesaurus.py
+++ canons/thesaurus.py
@@ -6,6 +6,7 @@
 """
 from canons import altervista
 from canons import bighugelabs
+from canons import stands4
 from canons import watson
 from canons import wordnet
 from canons.utils import Thesaurus
@@ -13,6 +14,7 @@ from canons.utils import Thesaurus
 THESAURI = [
     Thesaurus(altervista.canon, 'canons/logs/altervista.json'),
     Thesaurus(bighugelabs.canon, 'canons/logs/bighugelabs.json'),
+    Thesaurus(stands4.canon, 'canons/logs/stands4.json'),
     Thesaurus(watson.canon, 'canons/logs/watson.json'),
     Thesaurus(wordnet.canon, 'canons/logs/wordnet.json')
     ]
